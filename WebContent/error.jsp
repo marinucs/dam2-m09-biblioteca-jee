7c7a07f49ba7d2b9b9a99397a77b5db0d4dddf96
Marina Ferrer <ferrergarridom@gmail.com>
2022-11-12 15:26:38 +0100

    implementado error.jsp y creada bienvenida.jsp

---
+++ WebContent/error.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+		<title>LOGIN</title>
+	</head>
+		<body>
+			<H1>ACCESO NO PERMITIDO USUARIO <%=request.getParameter("usuario") %></H1>
+		</body>
+</html>
--- src/servlet/GestorBibliotecaServlet.java
+++ src/servlet/GestorBibliotecaServlet.java
@@ -17,56 +17,59 @@ import javax.servlet.http.HttpServletResponse;
 public class GestorBibliotecaServlet extends HttpServlet {
 
 	private boolean yaIniciado = false;
-	
+
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
-		String usuario=request.getParameter("usuario");
-		String password=request.getParameter("password");
-		
-		if(usuario.equals("pepe") && password.equals("pepe")) {
-			response.setContentType("text/html;charset=UTF8");
-			try (PrintWriter out = response.getWriter()) {
-				out.println("<!DOCTYPE html>");
-				out.println("<html><head><title>Biblioteca J2EE</title></head>");
-				out.println("<body>");
-					if(!yaIniciado) {
+
+		String usuario = request.getParameter("usuario");
+		String password = request.getParameter("password");
+
+		if (usuario != null && password != null) {
+			if (usuario.equals("pepe") && password.equals("pepe")) {
+				response.setContentType("text/html;charset=UTF8");
+				try (PrintWriter out = response.getWriter()) {
+					out.println("<!DOCTYPE html>");
+					out.println("<html><head><title>Biblioteca J2EE</title></head>");
+					out.println("<body>");
+					if (!yaIniciado) {
 						yaIniciado = true;
 						out.println("<h2>Conectado a la base de datos (GET)</h2>");
 					}
 					out.println("<h1>HOLA MUNDO GET</h1>");
 					out.println("</body>");
 					out.println("</html>");
-			}
-		} else response.sendRedirect("error.html");
-
+				}
+			} else
+				response.sendRedirect("error.jsp?usuario=" + usuario);
+		}
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
-		String usuario=request.getParameter("usuario");
-		String password=request.getParameter("password");
+
+		String usuario = request.getParameter("usuario");
+		String password = request.getParameter("password");
 		response.setContentType("text/html;charset=UTF8");
 
 		try (PrintWriter out = response.getWriter()) {
 			out.println("<!DOCTYPE html>");
 			out.println("<html><head><title>Biblioteca J2EE</title></head>");
 			out.println("<body>");
-			if(usuario.equals("pepe") && password.equals("pepe")) {
-				if(!yaIniciado) {
+			if (usuario.equals("pepe") && password.equals("pepe")) {
+				if (!yaIniciado) {
 					yaIniciado = true;
 					out.println("<h2>Conectado a la base de datos (POST)</h2>");
 				}
 				out.println("<h1>HOLA MUNDO POST</h1>");
-			} else out.println("<h1> ACCESO NO PERMITIDO USUARIO " + usuario + " </h1>");
+			} else
+				out.println("<h1> ACCESO NO PERMITIDO USUARIO " + usuario + " </h1>");
 			out.println("</body>");
 			out.println("</html>");
 
 		}
-		
+
 	}
 
 }
